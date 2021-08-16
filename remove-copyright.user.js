c44e84ef8a866e4aea7ffc14d40ca4c0ba8f44ad
Su Yifan <sorashu@outlook.com>
2021-08-16 18:15:44 +0800

    init

---
+++ remove-copyright.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         remove-copyright
+// @namespace    https://github.com/SoraShu/remove-copyright
+// @version      0.1
+// @description  移除复制时的copyright信息，净化剪贴板
+// @author       SoraShu
+// @match        *://blog.csdn.net/*/article/details/*
+// @match        *://*.blog.csdn.net/article/details/*
+// @match        *://www.zhihu.com/*
+// @match        *://zhuanlan.zhihu.com/*
+// @match        *://leetcode-cn.com/problems/*
+// @icon         https://z3.ax1x.com/2021/08/16/fWMOMT.png
+// @grant        none
+// @run-at document-start
+// ==/UserScript==
+
+
+//随便写的，其实也就一行代码
+//将保留原格式，不影响将复制的内容直接贴到Typora里
+//目前支持知乎和CSDN
+(function() {
+    'use strict';
+    document.oncopy = event => event.clipboardData.setData('text', window.getSelection(0).toString()); //es6 yyds
+})();
